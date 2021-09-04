788527a5b47f932f2b9a404f5a90796e7c85d23d
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-09-04 21:43:43 +0530

    #include<iostream> added

--- stdc++.h
+++ stdc++.h
@@ -28,94 +28,95 @@
 
 // Also writing "using namespace std;" here so that you dont need to write it everytime you start a cpp file
 
+#include <iostream>
 using namespace std;
 
 
-  // 17.4.1.2 Headers
+// 17.4.1.2 Headers
 
-  // C
-  #ifndef _GLIBCXX_NO_ASSERT
-  #include <cassert>
-  #endif
-  #include <cctype>
-  #include <cerrno>
-  #include <cfloat>
-  #include <ciso646>
-  #include <climits>
-  #include <clocale>
-  #include <cmath>
-  #include <csetjmp>
-  #include <csignal>
-  #include <cstdarg>
-  #include <cstddef>
-  #include <cstdio>
-  #include <cstdlib>
-  #include <cstring>
-  #include <ctime>
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
-  #if __cplusplus >= 201103L
-  #include <ccomplex>
-  #include <cfenv>
-  #include <cinttypes>
-  #include <cstdbool>
-  #include <cstdint>
-  #include <ctgmath>
-  #include <cwchar>
-  #include <cwctype>
-  #endif
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
 
-  // C++
-  #include <algorithm>
-  #include <bitset>
-  #include <complex>
-  #include <deque>
-  #include <exception>
-  #include <fstream>
-  #include <functional>
-  #include <iomanip>
-  #include <ios>
-  #include <iosfwd>
-  #include <iostream>
-  #include <istream>
-  #include <iterator>
-  #include <limits>
-  #include <list>
-  #include <locale>
-  #include <map>
-  #include <memory>
-  #include <new>
-  #include <numeric>
-  #include <ostream>
-  #include <queue>
-  #include <set>
-  #include <sstream>
-  #include <stack>
-  #include <stdexcept>
-  #include <streambuf>
-  #include <string>
-  #include <typeinfo>
-  #include <utility>
-  #include <valarray>
-  #include <vector>
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
 
-  #if __cplusplus >= 201103L
-  #include <array>
-  #include <atomic>
-  #include <chrono>
-  #include <condition_variable>
-  #include <forward_list>
-  #include <future>
-  #include <initializer_list>
-  #include <mutex>
-  #include <random>
-  #include <ratio>
-  #include <regex>
-  #include <scoped_allocator>
-  #include <system_error>
-  #include <thread>
-  #include <tuple>
-  #include <typeindex>
-  #include <type_traits>
-  #include <unordered_map>
-  #include <unordered_set>
-  #endif
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
